3253bcc37e01e35ddf20ab14147fb1309ccf2d64
daseeds <cyril@cyrils-MacBook-Pro.local>
2015-02-27 09:31:13 +0100

    first commit

---
+++ apps/kv_server/lib/kv_server.ex
@@ -0,0 +1,55 @@
+defmodule KVServer do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def accept(port) do
+  # The options below mean:
+  #
+  # 1. `:binary` - receives data as binaries (instead of lists)
+  # 2. `packet: :line` - receives data line by line
+  # 3. `active: false` - block on `:gen_tcp.recv/2` until data is available
+  #
+  {:ok, socket} = :gen_tcp.listen(port,
+                    [:binary, packet: :line, active: false])
+  IO.puts "Accepting connections on port #{port}"
+  loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    loop_acceptor(socket)
+  end
+
+  defp serve(client) do
+    client
+    |> read_line()
+    |> write_line(client)
+
+    serve(client)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+end
